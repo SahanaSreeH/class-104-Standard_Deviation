dd0451ee75d12ee9a9aba0d7cbcd83fba512679a
SahanaSreeH <77058482+SahanaSreeH@users.noreply.github.com>
2021-12-10 18:55:14 +0530

    Add files via upload

---
+++ class-105-Standard_deviation/solution/std_deviation.py
@@ -0,0 +1,43 @@
+
+import math
+
+
+
+# list of elements to calculate mean
+import csv
+with open('C:\Projects\class-105-Standard_deviation\solution\data.csv', newline='') as f:
+    reader = csv.reader(f)
+    file_data = list(reader)
+
+
+data = file_data[0]
+
+#getting the mean
+# data = [20,69,56,90,40,99,86,100,70,69,80,65,57,82,90,70,79,39,90,80,86,53,97,95,88,47,100,56,97,100] #list of x or y
+# data=[60,61,65,63,98,99,90,95,91,96]
+
+# finding mean
+def mean(data):
+    n= len(data)
+    total =0
+    for x in data:
+        total += int(x)
+
+    mean = total / n
+    return mean
+
+square_list = []
+for num in data:
+    value = int(num) - mean(data)
+    value = value * value
+    square_list.append(value)
+
+sum = 0
+for s in square_list:
+    sum += s
+
+print(sum)
+
+result = sum / (len(data)-1)
+std_deviation = math.sqrt(result)
+print(std_deviation)
